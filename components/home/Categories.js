ade9e6033de69baae5242a8027af23702086c09d
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-07-04 17:10:25 +0530

    ctegory added

---
+++ components/home/Categories.js
@@ -0,0 +1,41 @@
+import { View, Text, Image, ScrollView } from 'react-native'
+import React from 'react'
+
+const items = [
+    {
+      image: require("../../assets/images/shopping-bag.png"),
+      text: "Pick-up",
+    },
+    {
+      image: require("../../assets/images/soft-drink.png"),
+      text: "Soft Drinks",
+    },
+    {
+      image: require("../../assets/images/bread.png"),
+      text: "Bakery Items",
+    },
+    {
+      image: require("../../assets/images/fast-food.png"),
+      text: "Fast Foods",
+    },
+    {
+      image: require("../../assets/images/deals.png"),
+      text: "Deals",
+    },
+    {
+      image: require("../../assets/images/coffee.png"),
+      text: "Coffee & Tea",
+    },
+    {
+      image: require("../../assets/images/desserts.png"),
+      text: "Desserts",
+    },
+  ];
+
+export default function Categories() {
+  return (
+    <View>
+      <Image>Categories</Image>
+    </View>
+  )
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,17 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
-import HeaderTabs from '../components/HeaderTabs'
+import HeaderTabs from '../components/home/HeaderTabs'
+import SearchBar from '../components/home/SearchBar'
+import Categories from '../components/home/Categories'
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <HeaderTabs style={{marginHorizontal: 200}}/>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+    <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs style={{marginHorizontal: 200}}/>
+        <SearchBar/>
+    </View>
+        <Categories/>
     </SafeAreaView>
   )
 }
