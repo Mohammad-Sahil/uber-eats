d6c9fb71c78f9f24820eed9578f923c19be3e85b
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-07-04 17:17:04 +0530

    categories complete

--- components/home/Categories.js
+++ components/home/Categories.js
@@ -34,8 +34,29 @@ const items = [
 
 export default function Categories() {
   return (
-    <View>
-      <Image>Categories</Image>
+    <View
+      style={{
+        marginTop: 5,
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+        paddingLeft: 20,
+      }}
+    >
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map((item, index) => (
+          <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+            <Image
+              source={item.image}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: "contain",
+              }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   )
 }
